89f3c37e79efcb7cb1677eab565c1ada7305ea0d
4kalol <yuya.shikata.work@gmail.com>
2024-04-26 17:03:10 +0900

    Googleログイン

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -86,5 +86,10 @@ const submit = () => {
                 </PrimaryButton>
             </div>
         </form>
+        <div class="flex items-center justify-end mt-4">
+            <Link :href="route('login.google')" class="ml-3 inline-flex items-center">
+                <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" style="margin-left: 3em;">
+            </Link>
+        </div>
     </GuestLayout>
 </template>
